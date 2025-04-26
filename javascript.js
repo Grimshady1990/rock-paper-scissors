1831bf59780423d63d3d349fa1ec7060ba1835f2
Grimshady1990 <frankiedthomas@gmail.com>
2025-04-26 13:25:48 +0100

    Create a function that generates 3 random numbers

--- javascript.js
+++ javascript.js
@@ -1,2 +1,21 @@
 // Test script is working correctly
 console.log("TEST");
+
+/* ---------------STEP 1-----------------
+   -------Create a computer that--------- 
+   ---------can play the game------------ */
+
+// CREATE a Function
+function getComputerChoice() {
+
+// ADD a variable that accepts 3 random numbers
+
+// ASSIGN string values to the numbers
+
+// RETURN the results
+    return Math.floor(Math.random() * 3);
+
+}
+
+console.log(getComputerChoice());
+
